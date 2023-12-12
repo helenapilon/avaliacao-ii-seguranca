6358d862f44fc91e076fd5394a84ac3229ce97f4
helena <hppilon@gmail.com>
2023-12-12 18:36:56 -0300

    frontend files

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import styles from "./styles.module.css";
+export default function Card({ titulo, preco, descricao, id }) {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <p>{titulo}</p>
+      <img
+        className={styles.cardImg}
+        src="https://img.freepik.com/vetores-premium/padrao-sem-emenda_1159-5086.jpg"
+      />
+      <span className={styles.descricao}>{descricao}</span>
+      <span>
+        {preco?.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+      </span>
+      <button>Ver mais</button>
+    </div>
+  );
+}
